6ac6041d2b63957dea316a38eff19a287c53e5ea
mahmud-reza-rafsun <rafsun16.it@gmail.com.com>
2025-04-27 11:49:32 +0600

    Environment Variables added

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAbSl2vg8FchfkU-GonbMmAzb87hy9KMI4",
-  authDomain: "winter-clothing-donation-5cb28.firebaseapp.com",
-  projectId: "winter-clothing-donation-5cb28",
-  storageBucket: "winter-clothing-donation-5cb28.firebasestorage.app",
-  messagingSenderId: "431112929622",
-  appId: "1:431112929622:web:2125fd7e5b57aa7e2856f7"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
